1b6ab2e86546c71a930a74b4d3bfbf21e1fec699
mohammadariful20 <arifulz848@gmail.com>
2024-03-26 05:25:23 +0600

    project setup and footer doen

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+
+export default function Root() {
+  return (
+    <div>
+      
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+}
